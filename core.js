16f4613b454bbc446b7e2030db07213ae44455df
Django-Argus <malaska@mailo.com>
2025-04-02 21:00:16 +0200

    Update

--- core.js
+++ core.js
@@ -2,7 +2,12 @@
 const WIDTH = 30;
 const HEIGHT = 20;
 
-const COLORS = ["a", "b", "c", "d", "e"]
+const COLORS = ["#6d001a", "#be0039", "#ff4500", "#ffa800", "#ffd635","#fff8b8",
+                "#00a368", "#00cc78", "#7eed56", "#00756f", "#009eaa", "#00ccc0",
+                "#2450a4", "#3690ea", "#51e9f4", "#493ac1", "#6a5cff", "#94b3ff",
+                "#811e9f", "#b44ac0", "#e4abff", "#de107f", "#ff3881", "#ff99aa",
+                "#6d482f", "#9c6926", "#ffb470", "#000000", "#515252", "#898d90",
+                "#d4d7d9", "#ffffff"];
 
 var PIXELS = [];
 for(let y = 0; y < HEIGHT; y++) {
@@ -12,14 +17,44 @@ for(let y = 0; y < HEIGHT; y++) {
     }
 }
 
+var HISTORY = [];
+
 /**
  * 
  * @param {Array} coord 
  * @param {Int} color 
  */
 function setPixel(coord, color) {
-    if(coord == undefined || color == undefined || !Array.isArray(coord) || coord.length != 2 || coord[0] < 0 || coord[1] < 0 || coord[0] >= WIDTH || coord[1] >= HEIGHT)
+    if(coord == undefined || color == undefined || !Array.isArray(coord) || coord.length != 2 || coord[0] < 0 || coord[1] < 0 || color < 0 || coord[0] >= WIDTH || coord[1] >= HEIGHT || color >= COLORS.length)
         return;
+    
+    addHistoryEntry(coord, color);
+    PIXELS[coord[1]][coord[0]] = color;
+}
 
-    PIXELS[coord[1]][coord[0]] = color
+function addHistoryEntry(coord, color) {
+    let key = coord[0] + ":" + coord[1];
+    let date = new Date().getTime();
+
+    HISTORY[key] = {
+        time: date,
+        coord: coord,
+        color: color
+    }
+}
+
+function getPeriodicData() {
+    let data = Object.values(HISTORY);
+    HISTORY = [];
+    return data;
+}
+
+function getLoginData() {
+    return {pixels: PIXELS, colors: COLORS};
+}
+
+module.exports = {
+    setPixel,
+    getLoginData,
+    getPeriodicData
 }
--- server.js
+++ server.js
@@ -1,11 +1,72 @@
 const {WebSocketServer} = require("ws");
+const core = require("./core");
+
+const PERIODIC_SEND_TIMEOUT = 3000;
+const TYPES = {LOGIN: 0, PLACE: 1};
 
 /**
  * Trigger de connexion client
  * @param {WebSocket} clientSocket 
  */
 function connection(clientSocket) {
+    clientSocket.on("message", (data) => {
+        try {
+            let jsonData = JSON.parse(data);
+            onClientData(jsonData, clientSocket);
+        }catch(e) {
+            if(e.name != 'SyntaxError') {
+                console.error(e);
+            }
+        }
+    });
+}
+
+function onClientData(data, socket) {
+    if(!Object.values(TYPES).includes(data.type))
+        return;
     
+    switch(data.type) {
+        case TYPES.LOGIN:
+            onLogin(socket);
+            break;
+        case TYPES.PLACE:
+            onPlace(data, socket);
+            break;
+    }
+}
+
+function onLogin(socket) {
+    let loginData = core.getLoginData();
+    loginData.type = TYPES.LOGIN;
+    send(loginData, socket);
+}
+
+function onPlace(data, socket) {
+    if(data.coord == undefined || data.color == undefined || !Array.isArray(data.coord)) {
+        return;
+    }
+
+    core.setPixel(data.coord, data.color);
+}
+
+function sendPeriodic(server) {
+    let data = core.getPeriodicData();
+    if(data == undefined || data.length == 0) {
+        return;
+    }
+    
+    broadcast(data, server);
+}
+
+function broadcast(data, server) {  
+    server.clients.forEach(socket => {
+        socket.send(JSON.stringify(data));
+        
+    });  
+}
+
+function send(data, socket) {    
+    socket.send(JSON.stringify(data));
 }
 
 /**
@@ -14,5 +75,9 @@ function connection(clientSocket) {
  */
 module.exports = (port) => {
     const server = new WebSocketServer({port: port});
+    server.on("listening", () => {
+        console.log("WebSocket open on port: " + port);
+        setInterval(sendPeriodic, PERIODIC_SEND_TIMEOUT, server);
+    })
     server.on("connection", connection);
 };
